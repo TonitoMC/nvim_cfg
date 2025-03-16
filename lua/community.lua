6c5f601c4d8a8cf6c77f7c81c55eaef7d92750bc
Jose Merida <joseameridac@gmail.com>
2025-03-16 00:56:28 -0600

    Initial Config

---
+++ lua/community.lua
@@ -0,0 +1,18 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python"},
+  { import = "astrocommunity.pack.go"},
+  { import = "astrocommunity.pack.html-css"},
+  { import = "astrocommunity.pack.docker"},
+  { import = "astrocommunity.pack.svelte"},
+  { import = "astrocommunity.pack.rust"},
+  { import = "astrocommunity.game.leetcode-nvim"},
+  { import = "astrocommunity.diagnostics.trouble-nvim"},
+  { import = "astrocommunity.workflow.hardtime-nvim"}
+}
---
+++ lua/plugins/catpuccin.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      dim_inactive = { enabled = true, percentage = 0.25 },
+      transparent_background = false,
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
